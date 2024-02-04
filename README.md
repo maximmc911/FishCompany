e60cf93affd4f98b15e4620046c22fc7fb84ed57
max <89175221282@mail.ru>
2024-02-04 23:33:19 +0300

    example

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 
-
+[ссылка](src/components/footer/Footer.tsx)
 # React + TypeScript + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/Pages/PartnerPages/Pages/MessagePartner.tsx
+++ src/Pages/PartnerPages/Pages/MessagePartner.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MainMessage from '../../../components/messagesPage/MainMessage'
+import Mail from '../../../components/messagesPage/Mail'
 
 const MessagePartner = () => {
   return (
     <>
       <h1 className='p-5 text-2xl'>Обратная связь и почта</h1>
-      <MainMessage message={`клиент`}/>
+      {/* <MainMessage message={`клиент`}/> */}
+      <Mail/>
     </>
   )
 }
